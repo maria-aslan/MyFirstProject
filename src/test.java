eae515312cc7e8f8025d4dc042a4469dd4438449
maria <aslnalv25@icloud.com>
2022-12-27 20:50:40 -0500

    this is my first commit

--- src/test.java
+++ src/test.java
@@ -1,3 +1,16 @@
 this is my first line
         this is second line
 i added this line from GitHub
+
+
+
+commit and push -> when you change something from your laptop,
+to send them to GitHub (server) , you will use commit and push button.
+
+commit-> save my change to my local repo
+push -> sending your commited changes to github(server)
+
+if you want to get the latest code from server
+Git-pull
+
+pull-> pull the lastes changes from the github(server)
